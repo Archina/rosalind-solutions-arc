4a682721267d8e3a95349a50524dfcfaf418f3c6
Archina <archina5thrider@gmail.com>
2024-03-24 16:42:51 +0000

    put in order, also ignore because solved

--- src/_iprb.rs
+++ src/_iprb.rs
@@ -1,3 +1,9 @@
+#[ignore]
+#[test]
+fn asset_example_problem() {
+    assert_eq!(probability_for_dominant_trait(2, 2, 2), 0.7833333333333332)
+}
+
 // Problem 09 IPRB - mendelian
 pub fn probability_for_dominant_trait(k: usize, m: usize, n: usize) -> f64 {
     let sum = k + m + n;
@@ -12,10 +18,4 @@ pub fn probability_for_dominant_trait(k: usize, m: usize, n: usize) -> f64 {
 
 fn probability(n: usize, out_of: usize) -> f64 {
     n as f64 / out_of as f64
-}
-
-#[ignore]
-#[test]
-fn asset_example_problem() {
-    assert_eq!(probability_for_dominant_trait(2, 2, 2), 0.7833333333333332)
 }
